774e27233f18bcdc578ecda4b2f8ec600ae5400c
Davee02 <cmwJKiqVvyliffEId7jJ>
2018-11-28 13:48:01 +0100

    Added Helper-class

---
+++ SwissTransport.App/Helper/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace SwissTransport.App.Helper
+{
+    public static class Helper
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> source)
+        {
+            return new ObservableCollection<T>(source);
+        }
+    }
+}
