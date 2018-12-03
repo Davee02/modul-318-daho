79b016614ce8db1845174ebd0164cb223815a2c2
Davee02 <cmwJKiqVvyliffEId7jJ>
2018-12-03 10:57:52 +0100

    Added class "GeoLocator" for location-servicec

---
+++ SwissTransport.App/Helper/GeoLocator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Device.Location;
+
+namespace SwissTransport.App.Helper
+{
+    public class GeoLocator
+    {
+        private readonly GeoCoordinateWatcher m_watcher;
+
+        public bool HasPermissions => m_watcher.Permission == GeoPositionPermission.Granted;
+        public bool LocatorIsReady => m_watcher.Status == GeoPositionStatus.Ready;
+
+        public GeoLocator()
+        {
+            m_watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.Default);
+        }
+
+        public bool StartLocator()
+        {
+            return m_watcher.TryStart(false, TimeSpan.FromSeconds(2));
+        }
+
+        public Coordinate GetLocation()
+        {
+            return new Coordinate
+            {
+                XCoordinate = m_watcher.Position.Location.Latitude,
+                YCoordinate = m_watcher.Position.Location.Longitude
+            };
+        }
+    }
+}
